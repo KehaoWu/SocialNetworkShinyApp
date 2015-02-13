5d64b688e86e6270addd264e45db2d22fc43bba0
KehaoWu <kehao.wu@gmail.com>
2015-02-13 10:28:15 +0800

    Chinese UI

---
+++ publish.R
@@ -0,0 +1,3 @@
+library(devtools)
+library(shinyapps)
+shinyapps::deployApp()
--- ui.R
+++ ui.R
@@ -1,6 +1,40 @@
 library(shiny)
 library(igraph)
 
+UIcharSetChinese = list(headTitle = "人物关系图",
+                        ok = "准备就绪？",
+                        person1 = "第一关系人（逗号(英文状态下半角)或者空格隔开）",
+                        person2 = "第二关系人（逗号(英文状态下半角)或者空格隔开）",
+                        RelationLabel = "关系标签（逗号(英文状态下半角)或者空格隔开）",
+                        mainTitle = "主标题",
+                        vcolor = "人物图形颜色",
+                        ecolor = "关系线颜色",
+                        mainFontSize = "主标题字体大小",
+                        vSize = "人物图形大小",
+                        eLWD = "关系线粗细",
+                        vfcolor = "人物标签字体颜色",
+                        efcolor = "关系标签字体颜色",
+                        vLabelFontSize = "人物标签字体大小",
+                        eLabelFontSize = "关系标签字体大小")
+
+UIcharSetEnglish = list(headTitle = "Social Relationship Network Graph",
+                        ok = "Are You Ready？",
+                        person1 = "First Persons (separed by space or comma)",
+                        person2 = "Second Persons (separed by space or comma)",
+                        RelationLabel = "Relationship Labels (separed by space or comma)",
+                        mainTitle = "Main Title",
+                        vcolor = "Color for Persons",
+                        ecolor = "Color for Lines",
+                        mainFontSize = "Color for Main Title",
+                        vSize = "Size for Persons",
+                        eLWD = "Size for Lines",
+                        vfcolor = "Color for Labels of Persons",
+                        efcolor = "Color for Labels of Lines",
+                        vLabelFontSize = "Font Size for Labels of Persons",
+                        eLabelFontSize = "Font Size for Labels of Lines")
+
+UIcharSet = UIcharSetChinese
+
 dataset <- data.frame(
   person1 = "Tom,Jerry,Smith,Kath,Jerry,Jason,Obama,Jason",
   person2 = "Jerry,Kath,Tom,Tom,Smith,Obama,Kath,Tom",
@@ -11,27 +45,28 @@ colorSet <- c("grey","skyblue","turquoise","tomato","tan","slateblue",
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Social Network"),
+  #headerPanel("Social Network"),
+  headerPanel("人物关系图"),
   
   sidebarPanel(
-    checkboxInput("OK","Are you ready?",F),
-    textInput("person1","Former Person",value=dataset$person1),
-    textInput("person2","Later Person",value=dataset$person2),
-    textInput("RelationLabel","Label",value=dataset$relation),
-    textInput("mainTitle","Title",value="Social Network"),
-    selectInput('vcolor', 'Circle Color', colorSet, selected = "turquoise"),
-    selectInput('ecolor', 'Line Color', colorSet, selected = "grey"),
-    sliderInput('mainFontSize', 'Title Font Size', min=1, max=10,
+    checkboxInput("OK",UIcharSet$ok,F),
+    textInput("person1",UIcharSet$person1,value=dataset$person1),
+    textInput("person2",UIcharSet$person2,value=dataset$person2),
+    textInput("RelationLabel",UIcharSet$RelationLabel,value=dataset$relation),
+    textInput("mainTitle",UIcharSet$mainTitle,value="Social Network"),
+    selectInput('vcolor', UIcharSet$vcolor, colorSet, selected = "turquoise"),
+    selectInput('ecolor', UIcharSet$ecolor, colorSet, selected = "grey"),
+    sliderInput('mainFontSize', UIcharSet$mainFontSize, min=1, max=10,
                 value=3, step=0.2, round=0),
-    sliderInput('vSize', 'Size', min=10, max=100,
+    sliderInput('vSize', UIcharSet$vSize, min=10, max=100,
                 value=30, step=1, round=0),
-    sliderInput('eLWD', 'Line Width', min=1, max=10,
+    sliderInput('eLWD', UIcharSet$eLWD, min=1, max=10,
                 value=3, step=0.2, round=0),
-    selectInput('vfcolor', 'Circle Font Color', colorSet,selected="black"),
-    selectInput('efcolor', 'Line Font Color', colorSet,selected="black"),
-    sliderInput('vLabelFontSize', 'Circle Font Size', min=1, max=10,
+    selectInput('vfcolor', UIcharSet$vfcolor, colorSet,selected="black"),
+    selectInput('efcolor', UIcharSet$efcolor, colorSet,selected="black"),
+    sliderInput('vLabelFontSize', UIcharSet$vLabelFontSize, min=1, max=10,
                 value=2, step=0.2, round=0),
-    sliderInput('eLabelFontSize', 'Line Font Size', min=1, max=10,
+    sliderInput('eLabelFontSize', UIcharSet$eLabelFontSize, min=1, max=10,
                 value=2, step=0.2, round=0)
   ),
   
